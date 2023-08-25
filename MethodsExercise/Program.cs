25b41d39a8f6f79bbc8a87e9f484ccfc5b658e16
FrankAlves11 <Frankie.alves@icloud.com>
2023-08-25 11:22:42 -0700

    made changes

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          
+
+              Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+
+            Console.WriteLine("What is your favorite color?");
+            string favoriteColor = Console.ReadLine();
+            Console.WriteLine($"{name}, your favorite color is {favoriteColor}!");
+
+            Console.WriteLine("What is your favorite golf club to use?");
+            string favoriteClub = Console.ReadLine();
+            Console.WriteLine($"{name}, why is the {favoriteClub} your favorite golf club?");
+
+            string userResponse = Console.ReadLine();
+
+            Console.WriteLine("Nice, me too!");
+
+
+
+
+
         }
     }
 }
