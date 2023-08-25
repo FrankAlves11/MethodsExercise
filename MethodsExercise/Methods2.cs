6dfbca83fef513eefda621cf55782fffbeb96509
FrankAlves11 <Frankie.alves@icloud.com>
2023-08-25 13:07:40 -0700

    made changes

---
+++ MethodsExercise/Methods2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MethodsExercise
+{
+    internal class Methods2
+    {
+        public static int Sum(int num1, int num2)
+        {
+            int add = num2 + num1;
+            return add;
+
+
+
+
+        }
+        public static int Multi(int num1, int num2)
+        
+        {
+            int multi = num2 * num1;
+            return multi;
+        }
+
+      
+            
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
